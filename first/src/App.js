d34fce624aaeda91e9fd2baed876d09bf9862009
Dhruv <maindola.dhruv@gmail.com>
2023-02-11 01:20:19 +0530

    minor changes of few lines only

--- first/src/App.js
+++ first/src/App.js
@@ -1,13 +1,26 @@
-import "./App.css";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
+const RegisterForm = () => {
+  const { register, handleSubmit } = useForm();
+  const handleRegistration = (data) => console.log(data);
+
   return (
-    <div id="div1" className="App">
-      <header className="App-header">
-        <h1>Welcome to the login page for the game.</h1>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(handleRegistration)}>
+      <div>
+        <label>Name</label>
+        <input name="name" {...register("name")} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" {...register("email")} />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" {...register("password")} />
+      </div>
+      <button>Submit</button>
+    </form>
   );
-}
-
-export default App;
+};
+export default RegisterForm;
